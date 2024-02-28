1e4ebf9f6d1afb610541a018cc63c1ebd2f6c884
David Wittenbrink <david@dotdigital.dev>
2024-02-28 14:12:50 +0100

    Set tailwind lineHeight config instead of fontLeading

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ const colors = tokensToTailwind(colorTokens.items);
 const fontFamily = tokensToTailwind(fontTokens.items);
 const fontWeight = tokensToTailwind(textWeightTokens.items);
 const fontSize = tokensToTailwind(clampGenerator(textSizeTokens.items));
-const fontLeading = tokensToTailwind(textLeadingTokens.items);
+const lineHeight = tokensToTailwind(textLeadingTokens.items);
 const spacing = tokensToTailwind(clampGenerator(spacingTokens.items));
 
 module.exports = {
@@ -36,7 +36,7 @@ module.exports = {
     colors,
     spacing,
     fontSize,
-    fontLeading,
+    lineHeight,
     fontFamily,
     fontWeight,
     backgroundColor: ({theme}) => theme('colors'),
